f2cbc10ecfa4e1c65af483f2ff9163eca9f16b5d
zhangleio <zhangleio@outlook.com>
2018-04-16 15:47:24 +0800

    Added the webpack command for building umd package.

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+﻿var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    devtool: 'source-map',
+    entry: {
+        "app": './csg.js'
+    },
+    output: {
+        filename: 'csg.js',
+        path: path.resolve(__dirname, 'build'),
+        libraryTarget: 'umd',
+        library: 'csg'
+    }
+};
